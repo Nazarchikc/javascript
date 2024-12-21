b996f598133400818763eaa4a383ddb9c2129a6b
Nazarchikc <150057133+Nazarchikc@users.noreply.github.com>
2024-12-21 16:57:41 +0200

    home work 2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+const floatString = "123.45";
+const floatNumber = parseFloat(floatString);
+console.log("Десяткове число:", floatNumber);
+
+
+const integerString = "456";
+const integerNumber = parseInt(integerString);
+console.log("Ціле число:", integerNumber);
+
+
+const number = 64;
+const squareRoot = Math.sqrt(number);
+console.log("Квадратний корінь:", squareRoot);
+
+
+const intValue = 789;
+const stringValue = "123";
+const parsedInteger = parseInt(stringValue);
+console.log("Ціле число з рядка:", parsedInteger);
+
+const stringFromInt = intValue.toString();
+console.log("Рядок з цілого числа:", stringFromInt);
