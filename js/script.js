1c150683e7831e4965684f775dfe1714726a1875
Nazarchikc <150057133+Nazarchikc@users.noreply.github.com>
2024-12-22 12:26:00 +0200

    new hw2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 
+const daysInMonth = 30; 
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(`Кількість годин у ${daysInMonth} днях: ${hoursInMonth}`);
+console.log(`Кількість хвилин у ${daysInMonth} днях: ${minutesInMonth}`);
+
+
+const initialHealth = 100; 
+const initialEnergy = 75;  
+const health = initialHealth - 20; 
+const energy = initialEnergy - 15; 
+console.log(`Рівень здоров'я: ${health}`);
+console.log(`Рівень енергії: ${energy}`);
+
+
 const floatString = "123.45";
 const floatNumber = parseFloat(floatString);
 console.log("Десяткове число:", floatNumber);
+
+
+const totalPurchase = 500; 
+const discountedPrice = totalPurchase * 0.9;
+console.log(`Сума після застосування знижки 10%: ${discountedPrice}`);
+const floattNumber = 123.456;
+const roundedNumber = Math.floor(floatNumber);
+console.log(`Число ${floattNumber} після округлення вниз: ${roundedNumber}`);
 
 
 const integerString = "456";
