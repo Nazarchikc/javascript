4916ced4ad7f75d075705267834f43198455558a
Nazarchikc <150057133+Nazarchikc@users.noreply.github.com>
2025-01-08 19:22:02 +0200

    two lessen masivi

--- js/lesson/masivi.js
+++ js/lesson/masivi.js
@@ -22,15 +22,74 @@
 
 //  2
 // Створіть масив рядків. 
-const fruits = ["apple", "plum", "pear", "orange"];
+// const fruits = ["apple", "plum", "pear", "orange"];
 // Виведіть в консоль значення першого, 
 // другого та останнього елементу. 
 // Змініть значення останнього елементу на "peach"
 // а другого на "banana".
 
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[fruits.length-1]);
-fruits[fruits.length -1] =  "peach";
-fruits[1] = "banana";
-console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[fruits.length-1]);
+// fruits[fruits.length -1] =  "peach";
+// fruits[1] = "banana";
+// console.log(fruits);
+
+
+// for (let friend of myFriends) {
+//   console.log(friend);
+// }
+// console.log(myFriends);
+// for (let i = 0; i < myFriends.length; i += 1) {
+//   console.log(i);
+//   console.log(myFriends[i]);
+//   myFriends[i] = "fgh"
+// }
+
+
+
+const array = [10, 25, 13, 44, 15];
+// Вивести в консоль всі елементи масиву за допомогою 
+// циклів for та for…of.
+// const arrayLength = array.length;
+// for(let i = 0; i <array.length; i +=1) {
+   
+//     // console.log(array[arrayLength -1]);
+
+//     array[0];
+//     console.log( array[i]);
+    
+    
+// }
+
+
+// for( const item of array){
+//     console.log(item);
+    
+// }
+
+
+// 2
+// Порахувати загальну суму покупок в корзині за допомогою 
+// циклів for та for…of.
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+
+
+// // for (let i = 0; i < cart.length; i+=1) {
+// //     total += cart[i];
+// //   }
+
+
+// for (const item of cart) {
+//     total += item;
+//   }
+//   console.log(total);
+//   console.log(item);
+  
+  
+
+
+
+
