baa1fafa1b3abb5016eebce1de157327abaf1791
Nazarchikc <150057133+Nazarchikc@users.noreply.github.com>
2025-02-10 15:58:35 +0000

    lesson12

---
+++ js/lesson/lesson12.js
@@ -0,0 +1,63 @@
+// Створити стрілкову функцію hello1(), 
+// яка при визові буде вертати текст “Привіт JavaScript”. */
+// const Hello1 =() => {
+//     return "Hello Js";
+// }
+
+// const Hello1 =() => "Hello Js";
+
+// /*   2.
+// Напишіть стрілкову функцію hello2(), 
+// яка при виклику буде приймати змінну userName 
+// (наприклад, «Василь») і виводити рядок 
+// (в нашому випадку «Привіт, Василь»). */
+
+// const hello2 = (userName)  => "Привіт, " + userName
+// console.log(hello2('Vasil'));
+// console.log(hello2('Nazar'));
+// console.log(hello2('Matviy'));
+// console.log(hello2('Andriy'));
+
+
+/*   3.
+Напишіть стрілкову функцію mul(n, m), 
+яка приймає два аргументи і 
+повертає добуток, суму і різницю цих аргументів.
+ Перевірте її роботу. */
+
+
+//  const mul= (n, m) => {
+//     const multiply = n* m; 
+//     const total = n + m;
+//     const difference = n - m;
+//     return [multiply, total, difference]
+//  }
+// console.log(mul(6, 6));
+
+
+
+
+/*   4.
+Напиши стрілкову функцію myAverageScore , 
+яка у якості аргументу отримує масив з оцінками, 
+і виводить користувачу його середній результат 
+у наступному форматі:
+
+Averagescore: A (якщо середня оцінка від 91 до 100)
+Average score: B (якщо середня оцінка від 81 до 90)
+Average score: C (якщо середня оцінка від 71 до 80)
+Average score: D (якщо середня оцінка від 70 і менше)
+Для перевірки:*/
+// console.log(myAverageScore([100, 75, 81, 96]));
+// console.log(myAverageScore([45, 63, 85, 70]));
+
+const myAverageScore = (array) => {
+    let sum = 0
+    for (const nom of array){
+        sum += num
+    }
+}
+
+
+
+
